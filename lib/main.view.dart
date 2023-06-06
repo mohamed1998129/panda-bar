c1e7bb6c16bacea55f4a03cd4da44859254356e0
mohamed1998129 <younis1998129@gmail.com>
2023-06-06 14:01:53 +0300

    new

--- lib/main.view.dart
+++ lib/main.view.dart
@@ -49,72 +49,59 @@ class _PandaBarState extends State<PandaBar> {
 
   @override
   Widget build(BuildContext context) {
-    final clipper = _PandaBarClipper(fabSize: fabSize);
 
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        CustomPaint(
-          painter: _ClipShadowPainter(
-            shadow: Shadow(
-                color: Colors.white.withOpacity(.1),
-                blurRadius: 10,
-                offset: Offset(0, -3)),
-            clipper: clipper,
-          ),
-          child: ClipPath(
-            clipper: clipper,
-            child: Container(
-              height: 70,
-              padding: EdgeInsets.symmetric(vertical: 10),
-              color: widget.backgroundColor ?? Color(0xFF222427),
-              child: Builder(builder: (context) {
-                List<Widget> leadingChildren = [];
-                List<Widget> trailingChildren = [];
+        Container(
+          height: 70,
+          padding: EdgeInsets.symmetric(vertical: 10),
+          color: widget.backgroundColor ?? Color(0xFF222427),
+          child: Builder(builder: (context) {
+            List<Widget> leadingChildren = [];
+            List<Widget> trailingChildren = [];
 
-                widget.buttonData.asMap().forEach((i, data) {
-                  Widget btn = PandaBarButton(
-                    icon: data.icon,
-                    title: data.title,
-                    isSelected: data.id != null && selectedId == data.id,
-                    unselectedColor: widget.buttonColor,
-                    selectedColor: widget.buttonSelectedColor,
-                    onTap: () {
-                      setState(() {
-                        selectedId = data.id;
-                      });
-                      this.widget.onChange(data.id);
-                    },
-                  );
+            widget.buttonData.asMap().forEach((i, data) {
+              Widget btn = PandaBarButton(
+                icon: data.icon,
+                title: data.title,
+                isSelected: data.id != null && selectedId == data.id,
+                unselectedColor: widget.buttonColor,
+                selectedColor: widget.buttonSelectedColor,
+                onTap: () {
+                  setState(() {
+                    selectedId = data.id;
+                  });
+                  this.widget.onChange(data.id);
+                },
+              );
 
-                  if (i < 2) {
-                    leadingChildren.add(btn);
-                  } else {
-                    trailingChildren.add(btn);
-                  }
-                });
+              if (i < 2) {
+                leadingChildren.add(btn);
+              } else {
+                trailingChildren.add(btn);
+              }
+            });
 
-                return Row(
-                  children: [
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: leadingChildren,
-                      ),
-                    ),
-                    if(widget.hasFabIcon)
-                      Container(width: fabSize),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: trailingChildren,
-                      ),
-                    ),
-                  ],
-                );
-              }),
-            ),
-          ),
+            return Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: leadingChildren,
+                  ),
+                ),
+                if(widget.hasFabIcon)
+                  Container(width: fabSize),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: trailingChildren,
+                  ),
+                ),
+              ],
+            );
+          }),
         ),
         if(widget.hasFabIcon)
           PandaBarFabButton(
@@ -128,61 +115,3 @@ class _PandaBarState extends State<PandaBar> {
   }
 }
 
-class _PandaBarClipper extends CustomClipper<Path> {
-  final double fabSize;
-  final double padding = 50;
-  final double centerRadius = 25;
-  final double cornerRadius = 5;
-
-  _PandaBarClipper({this.fabSize = 100});
-
-  @override
-  Path getClip(Size size) {
-    final xCenter = (size.width / 2);
-
-    final fabSizeWithPadding = fabSize + padding;
-
-    final path = Path();
-    path.lineTo((xCenter - (fabSizeWithPadding / 2) - cornerRadius), 0);
-    path.quadraticBezierTo(xCenter - (fabSizeWithPadding / 2), 0,
-        (xCenter - (fabSizeWithPadding / 2)) + cornerRadius, cornerRadius);
-    path.lineTo(
-        xCenter - centerRadius, (fabSizeWithPadding / 2) - centerRadius);
-    path.quadraticBezierTo(xCenter, (fabSizeWithPadding / 2),
-        xCenter + centerRadius, (fabSizeWithPadding / 2) - centerRadius);
-    path.lineTo(
-        (xCenter + (fabSizeWithPadding / 2) - cornerRadius), cornerRadius);
-    path.quadraticBezierTo(xCenter + (fabSizeWithPadding / 2), 0,
-        (xCenter + (fabSizeWithPadding / 2) + cornerRadius), 0);
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, size.height);
-    path.lineTo(0, 0);
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(oldClipper) => false;
-
-}
-
-class _ClipShadowPainter extends CustomPainter {
-  final Shadow shadow;
-  final CustomClipper<Path> clipper;
-
-  _ClipShadowPainter({required this.shadow, required this.clipper});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = shadow.toPaint();
-    var clipPath = clipper.getClip(size).shift(shadow.offset);
-    canvas.drawPath(clipPath, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
